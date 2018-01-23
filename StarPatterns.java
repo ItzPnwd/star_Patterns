e457dbc66909158cee90b489eadd64a0edfe927b
ItzPnwd115 <itzpnwd115@gmail.com>
2018-01-22 20:00:52 -0500

    Need help

--- StarPatterns.java
+++ StarPatterns.java
@@ -101,11 +101,72 @@ class StarPatterns
         int x = w * 2;//j
         int y = h * 2;//i
         
-        for (int i=0; i<y; i++)
+        for (int i=0; i<x; i++)
         {
-            for (int j=0; j<x; j++)
+            for (int j=0; j<y; j++)
             {
-                if ((j < h && i < w)||(j < y && i < x))
+                if ((j < h && i < w)||(j >= h && i >= w))
+                {
+                    System.out.print("*");
+                }
+                
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void starTriangle(int h)
+    {
+        int w = h;
+        
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (j==0 || i== h-1 || i>=j)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void emptyTriangle(int h)
+    {
+        int w = h;
+        
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (j==0 || i== h-1 || i==j)
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }   
+    }
+    
+    public static void starTriangleUR(int h)
+    {
+        int w = h;
+        
+        for (int i=0; i<h; i++)
+        {
+            for (int j=0; j<w; j++)
+            {
+                if (i<=j)
                 {
                     System.out.print("*");
                 }
@@ -136,6 +197,14 @@ class StarPatterns
         
         twoStarBoxes(4,6);
         System.out.println();
+        
+        starTriangle(6);
+        System.out.println();
+        
+        emptyTriangle(6);
+        System.out.println();
+        
+        starTriangleUR(6);
+        System.out.println();
     }
-     
 }
